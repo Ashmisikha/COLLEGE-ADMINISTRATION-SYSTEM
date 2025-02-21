aaebd8a6f6ea3c9b5f846a2f0854647089c419de
ashmisikha piri <149249477+Ashmisikha@users.noreply.github.com>
2025-02-21 22:58:51 +0530

    Create student.java

---
+++ student.java
@@ -0,0 +1,22 @@
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private int id;
+    private String name;
+    private String department;
+    
+    public Student(int id, String name, String department) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getDepartment() { return department; }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Name: " + name + ", Dept: " + department;
+    }
+}
